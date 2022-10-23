86501ea7066f7e4cc4f4b52fa3bf0592959cb455
Joseph Chan <josephchan36@gmail.com>
2022-10-22 21:33:17 -0700

    added ready page

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+  
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
